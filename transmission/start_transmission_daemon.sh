814bef9a64f0c323aacc66f01eda93825fdd6573
Ioannis Triantafillou <johnrose@packer-debian-8-amd64.droplet.local>
2017-04-11 16:20:19 +0000

    Updates.

--- transmission/start_transmission_daemon.sh
+++ transmission/start_transmission_daemon.sh
@@ -1,5 +1,5 @@
-/usr/local/opt/transmission/bin/transmission-daemon \
+/usr/bin/transmission-daemon \
 --foreground \
 --log-info \
---logfile ~/small-site/plzmore/transmission/transmission-daemon.log \
---config-dir ~/small-site/plzmore/transmission/
+--logfile ~/my-projects/plzmore/transmission/transmission-daemon.log \
+--config-dir ~/my-projects/plzmore/transmission/
